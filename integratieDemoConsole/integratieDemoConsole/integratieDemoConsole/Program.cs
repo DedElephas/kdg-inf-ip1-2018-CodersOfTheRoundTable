98b01a7a1aca8262e9e05530f6e37c37288891f1
RogueWizardry <9131781+SeppeM@users.noreply.github.com>
2018-03-11 16:35:45 +0100

    Console read input

--- integratieDemoConsole/integratieDemoConsole/integratieDemoConsole/Program.cs
+++ integratieDemoConsole/integratieDemoConsole/integratieDemoConsole/Program.cs
@@ -11,17 +11,23 @@ namespace integratieDemoConsole
             Console.WriteLine("Demo Alerttriggering!");
            //1. MakeItem
            var BDW = new Item(1,"Bart De Wever",0.5,100);
-         //2. MakeUser
-           var SimonSomville = new User(1);
+            Console.WriteLine(BDW.naam + " zijn populariteit is " + BDW.populariteit);
+            //2. MakeUser
+            var SimonSomville = new User(1);
          //3. MakeAlert
            var userAlert = new Alert(1, 1, 0.75);
             //4. UpdateItem
             Console.Write("Hoeveel punten is " + BDW.naam + " gestegen in populariteit? aantal:");
             double stijging = Convert.ToDouble(Console.ReadLine());
-            BDW.populariteit = stijging;
-           //5. CheckAlert
-           //5.1 Trigger Alert
-           //6. AlertNotifiesUser
+            BDW.populariteit += stijging;
+            Console.WriteLine(BDW.naam + " is gestegen met " + stijging + " naar " + BDW.populariteit);
+            //5. CheckAlert
+            //5.1 Trigger Alert
+            //6. AlertNotifiesUser
+
+            // wacht op input voor het sluiten van de console
+            Console.WriteLine("Druk op ENTER voor de console te sluiten");
+            Console.ReadLine();
         }
     }
 }
