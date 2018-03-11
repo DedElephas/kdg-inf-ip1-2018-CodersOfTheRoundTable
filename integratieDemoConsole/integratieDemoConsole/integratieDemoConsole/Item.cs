2cbdb536d33c562f08c9f4e5e791d03c110d7292
Simon Somville <simon.somville@student.kdg.be>
2018-03-11 15:42:10 +0100

    Item

--- integratieDemoConsole/integratieDemoConsole/integratieDemoConsole/Item.cs
+++ integratieDemoConsole/integratieDemoConsole/integratieDemoConsole/Item.cs
@@ -6,5 +6,10 @@ namespace integratieDemoConsole
 {
     class Item
     {
-    }
+      public int itemId { get; set; }
+      public int type { get; set; }
+      public String naam { get; set; }
+      public int populariteit { get; set; }
+      public int vermelding { get; set; }
+   }
 }
