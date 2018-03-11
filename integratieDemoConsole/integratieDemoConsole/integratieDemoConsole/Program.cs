8b23aedb76382a061852382b605dc1efe29781c7
RogueWizardry <9131781+SeppeM@users.noreply.github.com>
2018-03-11 15:31:56 +0100

    Toegevoegd IntegratieDemoConsole

---
+++ integratieDemoConsole/integratieDemoConsole/integratieDemoConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace integratieDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Alerttriggering!");
+           //1. MakeItem
+           //2. MakeUser
+           //3. MakeAlert
+           //4. UpdateItem
+           //5. CheckAlert
+           //5.1 Trigger Alert
+           //6. AlertNotifiesUser
+        }
+    }
+}
