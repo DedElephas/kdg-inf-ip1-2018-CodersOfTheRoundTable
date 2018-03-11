f8592f1c2de42ee1a61319cf140d31f1bc1f491a
Simon Somville <simon.somville@student.kdg.be>
2018-03-11 16:40:35 +0100

    usred

--- integratieDemoConsole/integratieDemoConsole/integratieDemoConsole/User.cs
+++ integratieDemoConsole/integratieDemoConsole/integratieDemoConsole/User.cs
@@ -7,5 +7,15 @@ namespace integratieDemoConsole
 {
     class User
     {
-    }
+       private Domain.Registratie registratie;
+       private int registratieId;
+       private Alert userAlert;
+
+       public User(int registratieId)
+       {
+          this.registratie = new Registratie();
+          this.registratieId = registratieId;
+
+       }
+   }
 }
